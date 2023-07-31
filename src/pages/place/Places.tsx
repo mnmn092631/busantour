8f0e94336a5160b3034ce136b6fbc3a9d0407036
juhee kang <mnmn092631@gmail.com>
2023-07-31 22:31:21 +0900

    Subpage > Places

--- src/pages/place/Places.tsx
+++ src/pages/place/Places.tsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
+  Card,
+  CardCategory,
   CardContainer,
+  CardContent,
+  CardImg,
+  CardTitle,
   ContentContainer,
+  PageBtn,
+  Pagination,
   SelectContainer,
   SelectItem,
   Title,
@@ -21,7 +28,7 @@ interface PlaceData {
   subtitle: string;
   addr1: string;
   place_category: string;
-  tags: string | null;
+  tags: string;
   homepage_u: string;
   trfc_info: string;
   usage_day: string;
@@ -36,17 +43,25 @@ interface PlaceData {
 }
 
 const Places = () => {
+  const [places, setPlaces] = useState<PlaceData[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const offset = (page - 1) * 12;
   const [selectGugun, setSelectGugun] = useState<string>("전체");
   const gugun = ["전체", "북구", "강서구", "부산진구", "연제구", "서구", "금정구", "사상구", "동구", "영도구"];
 
   const getPlace = async () => {
     try {
       const response: PlaceData[] = await getData<PlaceData[]>("/busanplace");
+      setPlaces(response);
     } catch (error) {
       console.error(error);
       throw new Error("Failed to get user");
     }
   };
+
+  useEffect(() => {
+    getPlace();
+  }, []);
 
   return (
     <>
@@ -62,8 +77,58 @@ const Places = () => {
             </SelectItem>
           ))}
         </SelectContainer>
-        <CardContainer></CardContainer>
+        <CardContainer>
+          {places
+            .filter(place => {
+              if (selectGugun === "전체") return place;
+              else return place.gugun === selectGugun;
+            })
+            .slice(offset, offset + 12)
+            .map(place => (
+              <Card key={place.id}>
+                <CardImg src={place.main_img_n} alt={place.name} />
+                <CardTitle>
+                  <CardCategory>{place.place_category}</CardCategory>
+                  {place.name}
+                </CardTitle>
+                <CardContent>{place.addr1}</CardContent>
+              </Card>
+            ))}
+        </CardContainer>
       </ContentContainer>
+      <Pagination>
+        <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+          &lt;prev
+        </button>
+        {Array(
+          Math.ceil(
+            places.filter(place => {
+              if (selectGugun === "전체") return place;
+              else return place.gugun === selectGugun;
+            }).length / 12,
+          ),
+        )
+          .fill(null)
+          .map((_, i) => (
+            <PageBtn onClick={() => setPage(i + 1)} $active={i + 1 === page}>
+              {i + 1}
+            </PageBtn>
+          ))}
+        <button
+          onClick={() => setPage(page + 1)}
+          disabled={
+            page ===
+            Math.ceil(
+              places.filter(place => {
+                if (selectGugun === "전체") return place;
+                else return place.gugun === selectGugun;
+              }).length / 12,
+            )
+          }
+        >
+          next&gt;
+        </button>
+      </Pagination>
     </>
   );
 };
--- src/styles/pages/subpage/utils.ts
+++ src/styles/pages/subpage/utils.ts
@@ -22,12 +22,12 @@ export const Title = styled.h2`
 
 export const ContentContainer = styled.div`
   padding: 2% 16%;
-  height: 100vh;
 `;
 
 export const SelectContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+  margin-bottom: 20px;
 `;
 
 export const SelectItem = styled.span<{ $active: boolean }>`
@@ -56,4 +56,67 @@ export const SelectItem = styled.span<{ $active: boolean }>`
     `}
 `;
 
-export const CardContainer = styled.div``;
+export const CardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Card = styled.div`
+  margin-right: 2%;
+  margin-bottom: 2%;
+  width: 23%;
+  border-radius: 20px;
+  box-shadow: 10px 10px 15px #bebebe, -10px -10px 15px #ffffff;
+  overflow: hidden;
+  cursor: pointer;
+`;
+
+export const CardImg = styled.img`
+  width: 100%;
+  height: 60%;
+  margin-bottom: 10px;
+`;
+
+export const CardTitle = styled.h2`
+  text-align: center;
+  font-size: ${theme.fontSize.md};
+  font-weight: 600;
+  margin-bottom: 5px;
+`;
+
+export const CardCategory = styled.strong`
+  padding: 0 3px;
+  margin-right: 3px;
+  border-radius: 5px;
+  background-color: ${theme.color.blue};
+  color: ${theme.color.white};
+  font-size: ${theme.fontSize.sm};
+  font-weight: 400;
+`;
+
+export const CardContent = styled.p`
+  padding: 0 15px 10px;
+  text-align: center;
+  font-size: ${theme.fontSize.sm};
+`;
+
+export const Pagination = styled.div`
+  padding: 2% 16%;
+  width: 70vw;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const PageBtn = styled.div<{ $active?: boolean }>`
+  ${({ $active }) =>
+    $active &&
+    css`
+      color: ${theme.color.deepBlue};
+      font-weight: 600;
+      transform: scale(1.2);
+      transition: all 350ms ease-in-out;
+    `}
+  transition:all 350ms ease-in-out;
+  cursor: pointer;
+`;
