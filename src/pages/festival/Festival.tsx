e4f8c260323c411f73d4f2d064c3cae511d8ce93
juhee kang <mnmn092631@gmail.com>
2023-08-03 11:38:39 +0900

    하위 페이지 첫이미지 api 데이터로 변경

--- src/pages/festival/Festival.tsx
+++ src/pages/festival/Festival.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ContentContainer, Title, TitleContainer } from "src/styles/pages/subpage/utils";
-import tempimg1 from "../../assets/축제1.jpg";
+import { FestivalData } from "src/types/api";
+import apiService from "src/api";
 
 const Festival = () => {
+  const [festivals, setFestivals] = useState<FestivalData[]>();
+
+  useEffect(() => {
+    const getFestival = async () => {
+      try {
+        const response: FestivalData[] = await apiService.festivalService.getFestival();
+        setFestivals(response);
+      } catch (error) {
+        console.error(error);
+        throw new Error("Failed to get festival");
+      }
+    };
+    getFestival();
+  }, []);
+
   return (
     <>
       <TitleContainer>
-        <img src={tempimg1} alt="임시" />
+        {festivals && <img src={festivals[0].main_img} alt={festivals[0].name} />}
         <Title>지역축제</Title>
       </TitleContainer>
       <ContentContainer></ContentContainer>
--- src/pages/tour/Tour.tsx
+++ src/pages/tour/Tour.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ContentContainer, Title, TitleContainer } from "src/styles/pages/subpage/utils";
-import tempimg1 from "../../assets/축제3.jpg";
+import { TourData } from "src/types/api";
+import apiService from "src/api";
 
 const Tour = () => {
+  const [tours, setTours] = useState<TourData[]>();
+
+  useEffect(() => {
+    const getTour = async () => {
+      try {
+        const response: TourData[] = await apiService.tourService.getTour();
+        setTours(response);
+      } catch (error) {
+        console.error(error);
+        throw new Error("Failed to get tour");
+      }
+    };
+    getTour();
+  }, []);
+
   return (
     <>
       <TitleContainer>
-        <img src={tempimg1} alt="임시" />
+        {tours && <img src={tours[0].main_img_n} alt={tours[0].name} />}
         <Title>테마여행</Title>
       </TitleContainer>
       <ContentContainer></ContentContainer>
