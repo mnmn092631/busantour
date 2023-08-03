b45f58854e5b6fcc2ccf621b96f1d326f619fa9c
juhee kang <mnmn092631@gmail.com>
2023-08-03 17:57:24 +0900

    react-calendar 커스텀

--- src/components/subpage/CustomCalendar.tsx
+++ src/components/subpage/CustomCalendar.tsx
@@ -2,12 +2,20 @@ import React, { useState } from "react";
 import Calendar from "react-calendar";
 import { CalendarConatiner } from "src/styles/components/customCalendar";
 
+type ValuePiece = Date | null;
+
+export type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 const CustomCalendar = () => {
-  const [value, onChange] = useState(new Date());
+  const [value, onChange] = useState<ValuePiece | [ValuePiece, ValuePiece]>(new Date());
 
   return (
     <CalendarConatiner>
-      <Calendar value={value} />
+      <Calendar
+        onChange={onChange}
+        value={value}
+        formatDay={(locale, date) => date.toLocaleString("en", { day: "numeric" })}
+      />
     </CalendarConatiner>
   );
 };
--- src/styles/components/customCalendar.ts
+++ src/styles/components/customCalendar.ts
@@ -1,3 +1,25 @@
 import styled from "styled-components";
+import { theme } from "../theme";
 
-export const CalendarConatiner = styled.div``;
+export const CalendarConatiner = styled.div`
+  font-family: "Noto Sans KR", sans-serif;
+
+  .react-calendar {
+    width: 100%;
+  }
+
+  .react-calendar__navigation {
+    text-align: center;
+    display: flex;
+
+    & > button {
+      font-size: ${theme.fontSize.lg};
+      font-weight: 600;
+    }
+
+    .react-calendar__navigation__prev2-button,
+    .react-calendar__navigation__next2-button {
+      display: none;
+    }
+  }
+`;
