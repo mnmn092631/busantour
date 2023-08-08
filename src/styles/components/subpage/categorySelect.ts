f389a935bab545ef64b0e272d4e8001dc3c9391a
juhee kang <mnmn092631@gmail.com>
2023-08-08 14:05:19 +0900

    subpage > categorySelect 가운데 정렬, 글자 크기 수정

--- src/styles/components/subpage/categorySelect.ts
+++ src/styles/components/subpage/categorySelect.ts
@@ -2,10 +2,13 @@ import styled, { css } from "styled-components";
 import { media, theme } from "styles/theme";
 
 export const SelectContainer = styled.div`
+  margin: 0 auto;
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
   margin-bottom: 20px;
-  max-width: 75%;
+  max-width: 80%;
+
   ${media.tabletMin} {
     max-width: 100%;
   }
@@ -15,6 +18,7 @@ export const SelectItem = styled.span<{ $active: boolean }>`
   position: relative;
   padding-bottom: 5px;
   margin: 0 15px 5px 0;
+  font-size: ${theme.fontSize.lg};
   font-weight: 600;
   color: ${theme.color.gray};
   cursor: pointer;
